e2a97c540c71b6de4c24d72bea1dd98a23d5241b
Kevin Wanjala <wanjalahkevin97@gmail.com>
2024-03-19 14:20:02 +0300

    Added an arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1 = 1;
+const num2 = 62;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random()*Number.MAX_SAFE_INTEGER) + 1;
+const num3 = 87;
+const num4 = 83;
+const mod = (num3 % num4);
+const numbers =[4, 12, 20, 17];
+const max = Math.max(...numbers);
